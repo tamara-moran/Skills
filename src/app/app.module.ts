a4064362f8f9ee366a0e94b7a2c76b52feb8ab1a
Tamara-moran <tamara.moran@softwareone.com>
2022-02-22 13:35:35 +0100

    change boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './products/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ProductComponent]
 })
 export class AppModule { }
---
+++ src/app/products/product.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-product',
+    template: '<h2> Product List </h2>'
+})
+
+export class ProductComponent{}
